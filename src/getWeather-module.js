29fbbdd69a6643600a64a5669bf2399d5abfa18c
Guruh Edi Purwanto <guruhedipurwanto@gmail.com>
2020-01-29 20:14:25 +0800

    add function to display weather info

--- src/getWeather-module.js
+++ src/getWeather-module.js
@@ -1,11 +1,17 @@
-async function getWeather(location) {
+async function getWeatherData(location) {
   let weatherPromise = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=a988065929a71820ad27ba5b3420d64c`,
     { mode: 'cors' }
   )
-  let response = await weatherPromise.json()
-
-  return response
+  return await weatherPromise.json()
 }
 
-export { getWeather }
+function makeWeatherObject(weatherJson) {
+  return {
+    name: weatherJson.name,
+    weather: weatherJson.weather[0],
+    main: weatherJson.main,
+  }
+}
+
+export { getWeatherData, makeWeatherObject }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,40 @@
 import '@babel/polyfill'
-import { getWeather } from './getWeather-module.js'
+import { getWeatherData, makeWeatherObject } from './getWeather-module.js'
 
-getWeather('london')
+const weatherAppController = (function() {
+  async function displayWeather() {
+    const location = document.querySelector('input').value
+    document.querySelector('input').value = ''
+
+    let weatherData = await getWeatherData(location)
+
+    let weatherObject = makeWeatherObject(weatherData)
+    console.log(weatherObject)
+
+    document.querySelector('.cityname').textContent = weatherObject.name
+    document.querySelector('.weather').textContent =
+      weatherObject.weather.main + weatherObject.weather.description
+    document.querySelector('.temp').textContent = weatherObject.main.temp
+    document.querySelector('.feelslike').textContent =
+      weatherObject.main.feels_like
+    document.querySelector('.maxtemp').textContent = weatherObject.main.temp_max
+    document.querySelector('.mintemp').textContent = weatherObject.main.temp_min
+    document.querySelector('.pressure').textContent =
+      weatherObject.main.pressure
+    document.querySelector('.humidity').textContent =
+      weatherObject.main.humidity
+  }
+
+  return {
+    displayWeather,
+  }
+})()
+
+;(function() {
+  const getWeatherButton = document.querySelector('button')
+
+  getWeatherButton.addEventListener(
+    'click',
+    weatherAppController.displayWeather
+  )
+})()
